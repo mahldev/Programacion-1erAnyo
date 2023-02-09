8c21d38cc2994228888d8973415bceaef01a1626
HGH011 <114875813+HGH011@users.noreply.github.com>
2023-02-09 14:45:21 +0100

    He termindo el ejericio 7_4, queda el 7_6

--- Tema 7/Ar_7.5/src/Gestor.java
+++ Tema 7/Ar_7.5/src/Gestor.java
@@ -1,8 +1,8 @@
 public class Gestor {
-    
-    public String nombre;
+
+    private String nombre;
     private int telefono;
-    public double importeMaximo;
+    private double importeMaximo;
 
     Gestor(String nombre, int telefono, double importeMaximo) {
 
@@ -10,12 +10,34 @@ public class Gestor {
         this.telefono = telefono;
         this.importeMaximo = importeMaximo;
     }
-    
-    Gestor(String nombre, int telefono){
+
+    Gestor(String nombre, int telefono) {
         this.nombre = nombre;
         this.telefono = telefono;
         this.importeMaximo = 10000;
     }
 
-    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public double getImporteMaximo() {
+        return importeMaximo;
+    }
+
+    public void setImporteMaximo(double importeMaximo) {
+        this.importeMaximo = importeMaximo;
+    }
 }
