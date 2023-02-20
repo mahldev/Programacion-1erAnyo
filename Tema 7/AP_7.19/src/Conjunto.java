155a30eaeaffd54363b9a21876485f254f524bcf
HGH011 <114875813+HGH011@users.noreply.github.com>
2023-02-20 09:20:33 +0100

    He desarrollado las funciones union, inculuido. Pero hay que corregirlas

--- Tema 7/AP_7.19/src/Conjunto.java
+++ Tema 7/AP_7.19/src/Conjunto.java
@@ -1,6 +1,6 @@
 public class Conjunto {
 
-    private Integer[] datos;
+    public Integer[] datos;
     private int numeroElementos;
 
     public Conjunto() {
@@ -12,7 +12,7 @@ public class Conjunto {
     public Conjunto(int numeroElementos) {
 
         datos = new Integer[numeroElementos];
-        this.numeroElementos = numeroElementos;
+        this.numeroElementos = 0;
     }
 
     public int numeroElementos() {
@@ -60,25 +60,24 @@ public class Conjunto {
 
     private void comparaYElimina(Conjunto otroConjunto) {
 
-
         for (int indexThisConjunto = 0; indexThisConjunto < numeroElementos; indexThisConjunto++) {
 
             for (int indexOtroConjunto = 0; indexOtroConjunto < otroConjunto.numeroElementos; indexOtroConjunto++) {
 
                 if (datos[indexThisConjunto] == otroConjunto.datos[indexOtroConjunto]) {
 
-                    eliminarElemento(otroConjunto,otroConjunto.datos[indexOtroConjunto]);
+                    eliminarElemento(otroConjunto, otroConjunto.datos[indexOtroConjunto]);
                 }
             }
         }
     }
 
-    private boolean eliminarElemento(Conjunto conjunto ,Integer elemento) {
+    private boolean eliminarElemento(Conjunto conjunto, Integer elemento) {
 
         if (pertenece(elemento)) {
-            
+
             for (int index = 0; index < conjunto.numeroElementos; index++) {
-                
+
                 if (conjunto.datos[index] == elemento) {
 
                     conjunto.datos[index] = datos[conjunto.numeroElementos - 1];
@@ -93,9 +92,9 @@ public class Conjunto {
     public boolean eliminarElemento(Integer elemento) {
 
         if (pertenece(elemento)) {
-            
+
             for (int index = 0; index < numeroElementos; index++) {
-                
+
                 if (datos[index] == elemento) {
 
                     datos[index] = datos[numeroElementos - 1];
@@ -106,4 +105,58 @@ public class Conjunto {
         }
         return false;
     }
+
+    public static boolean incluido(Conjunto conjunto1, Conjunto conjunto2) {
+
+        boolean res = false;
+
+        for (int indexC1 = 0; indexC1 < conjunto1.numeroElementos; indexC1++) {
+
+            res = false;
+
+            for (int indexC2 = 0; indexC2 < conjunto2.numeroElementos; indexC2++) {
+
+                if (conjunto1.datos[indexC1] == conjunto2.datos[indexC2]) {
+
+                    res = true;
+                }
+            }
+            if (!res) {
+
+                return false;
+            }
+        }
+        return res;
+    }
+
+    public static Conjunto union(Conjunto conjunto1, Conjunto conjunto2) {
+
+        Conjunto nuevConjunto = new Conjunto(
+                conjunto1.numeroElementos + conjunto2.numeroElementos);
+
+        for (int index = 0; index < nuevConjunto.datos.length - 1; index++) {
+
+            nuevConjunto.datos[nuevConjunto.numeroElementos++] = conjunto1.datos[index];
+            nuevConjunto.datos[nuevConjunto.numeroElementos++] = conjunto2.datos[index];
+        }
+        return nuevConjunto;
+    }
+
+    private static int repetidos(Conjunto conjunto1, Conjunto conjunto2) {
+
+        int repetidos = 0;
+
+        for (int indexC1 = 0; indexC1 < conjunto1.numeroElementos; indexC1++) {
+
+            for (int indexC2 = 0; indexC2 < conjunto2.numeroElementos; indexC2++) {
+
+                if (conjunto1.datos[indexC1] == conjunto2.datos[indexC2]) {
+
+                    repetidos++;
+                }
+            }
+        }
+        return repetidos;
+    }
+
 }
