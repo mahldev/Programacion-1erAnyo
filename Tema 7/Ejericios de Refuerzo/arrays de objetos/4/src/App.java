54130f2c587d61c7125a15cce5d17eeec1b158ba
Your Name <you@example.com>
2023-03-05 16:57:59 +0100

    He empezado los ejercicios de array de objetos

---
+++ Tema 7/Ejericios de Refuerzo/arrays de objetos/4/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Disco[] discos = new Disco();
+        Scanner sc = new Scanner(System.in); 
+        int eleccion;
+
+        System.out.println("Seleccione una opcion");
+        System.out.println("1. Ver coleccion de discos");
+        System.out.println("2. Insertar nuevo disco");
+        System.out.println("3. Modificar disco");
+        System.out.println("4. Eliminar disco");
+        eleccion = sc.nextInt;
+
+        switch (eleccion) {
+            case 1 -> ; 
+            case 2 -> ; 
+            case 3 -> ; 
+            case 4 -> ; 
+        } 
+    }
+    
+}
---
+++ Tema 7/Ejericios de Refuerzo/arrays de objetos/4/src/Disco.java
@@ -0,0 +1,62 @@
+public class Disco {
+  
+  private String codigo = "LIBRE";
+  private String autor;
+  private String titulo;
+  private String genero;
+  private int duracion;
+
+  public Disco() {
+  } 
+
+  public Disco(String autor, String titulo, String genero, int duracion) {
+    this.autor = autor;
+    this.titulo = titulo;
+    this.genero = genero;
+    if (duracion > 0) {
+      this.duracion = duracion;
+    }
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public String getAutor() {
+    return this.autor;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getTitulo() {
+    return this.titulo;
+  }
+
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+
+  public String getGenero() {
+    return this.genero;
+  }
+
+  public void setDuracion(int duracion) {
+    this.duracion = duracion;
+  }
+
+  public String getDuracion() {
+    return this.duracion;
+  }
+
+  public String toString() {
+    String cadena = "\n------------------------------------------";
+    cadena += "\nCódigo: " + this.codigo;
+    cadena += "\nAutor: " + this.autor;
+    cadena += "\nTítulo: " + this.titulo;
+    cadena += "\nGénero: " + this.genero;
+    cadena += "\nDuración: " + this.duracion;
+    cadena += "\n------------------------------------------";
+    return cadena;
+}
