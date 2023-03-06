17f230a2dfacc68f915c52570c497264b981ce0f
HGH011 <114875813+HGH011@users.noreply.github.com>
2023-03-06 12:54:49 +0100

    He corregido el Tema 7, 14

--- Tema 8/Ejercicios de refuerzo/4/src/Terminal.java
+++ Tema 8/Ejercicios de refuerzo/4/src/Terminal.java
@@ -1,11 +1,3 @@
-
-/*
-Implementa la clase Terminal. Un terminal tiene asociado un número. Los
-terminales se pueden llamar unos a otros y el tiempo de conversación corre
-para ambos. A continuación se proporciona el contenido del main y el resultado
-que debe aparecer por pantalla.
- */
-
 public class Terminal {
 
     private String numero;
