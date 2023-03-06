e8899b9bbbd2b92cd69741d6ac9d9112daf37920
HGH011 <114875813+HGH011@users.noreply.github.com>
2023-03-06 10:29:15 +0100

    Hago cambios en la estructura

--- Tema 8/Ejercicios de refuerzo/4/src/Movil.java
+++ Tema 8/Ejercicios de refuerzo/4/src/Movil.java
@@ -1,14 +1,5 @@
 import java.text.DecimalFormat;
 
-/*Implementa la clase Movil como subclase de Terminal (la clase del ejercicio
-anterior que ya no hace falta modificar). Cada móvil lleva asociada una tarifa
-que puede ser “rata”, “mono” o “bisonte”. El coste por minuto es de 6, 12 y
-céntimos respectivamente. Se tarifican los segundos exactos. Obviamente,
-cuando un móvil llama a otro, se le cobra al que llama, no al que recibe la
-llamada. A continuación se proporciona el contenido del main y el resultado
-que debe aparecer por pantalla. Para que el total tarificado aparezca con dos
-decimales, puedes utilizar DecimalFormat. */
-
 public class Movil extends Terminal {
 
     private static final DecimalFormat df = new DecimalFormat("0.00");
