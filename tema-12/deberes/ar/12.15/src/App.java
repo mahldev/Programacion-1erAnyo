2fb03089e32586b7b803047feda2aeaf42d616f9
rzerpo <>
2023-05-08 18:46:05 +0200

    He terminado el ejericio 12.15

--- tema-12/deberes/ar/12.15/src/App.java
+++ tema-12/deberes/ar/12.15/src/App.java
@@ -12,16 +12,37 @@ public class App {
         return 0;
     }
 
+    private static Producto crearProducto() {
+        return new Producto(EntradaTeclado.pedirCadena("Introduzca el codigo: ")
+                            ,EntradaTeclado.pedirEntero("Introduzca el stock del producto: ") );
+    }
+
     public static void main(String[] args) throws Exception {
 
         Tienda tienda = new Tienda();
+        Producto p;
         int opcion;
 
-        switch ((opcion = menu())) {
-            case 1 -> {
-                
+        do {
+            switch ((opcion = menu())) {
+                case 1 -> {
+                    p = crearProducto();
+                    System.out.println(tienda.alta(p.getCodigo, p)
+                            ? "Se ha agregado correctamente"
+                            : "No se ha completado la operacion");
+                }
+                case 2 -> {
+                    System.out.println(tienda.baja(EntradaTeclado.pedirCadena("Introduzca el codigo del articulo: "))
+                            ? "Se ha eliminado correctamente"
+                            : "No se ha completado la operacion");
+                }
+                case 3 -> {
+                    System.out.println(tienda.actualizarStock(EntradaTeclado.pedirCadena("Introduzca el codigo del producto: "), EntradaTeclado.pedirEntero("Introduzca el nuevo stock: "))
+                    ? "Se ha modificado correctamente"
+                    : "No se ha completado la operacion");
+                }
             }
-        }
+        } while (opcion < 3);
     }
 
 }
